aeb8deea566e9c0ed5e1aa7e57823f5d5d1684bb
mSadik07 <sadikk.avciii@gmail.com>
2026-03-28 09:24:12 +0000

    güncelleme

--- easystock-api/index.js
+++ easystock-api/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // 1. CORS'u çağırdık
 const app = express();
 const port = 9000;
 
+app.use(cors()); // 2. Kapıları tüm tarayıcılara açtık
 app.use(express.json());
 
 mongoose.connect('mongodb://db:27017/EasyStockDB')
     .then(() => console.log("✅ MongoDB Bağlantısı Başarılı"))
     .catch(err => console.log("❌ MongoDB Hatası:", err));
 
-// Modelleri Kaydet (Sıralama önemli)
+// Modeller
 require('./app_api/models/User');
 require('./app_api/models/Product');
 
-// Rotaları Bağla
+// Rotalar
 const apiRoutes = require('./app_api/routes/index');
 app.use('/api', apiRoutes);
 
 app.listen(port, () => {
-    console.log(`🚀 EasyStock API http://localhost:${port} adresinde hazır!`);
+    console.log(`🚀 API Hazır: http://localhost:${port}`);
 });
